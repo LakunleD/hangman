03c83f7748b00f12e0b7be13eb29c07d619a9079
Olakunle Dosunmu <olakunledosunmu@gmail.com>
2023-09-09 16:46:52 +0100

    picking a word at random

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+extern crate rand;
+use rand::Rng;
+
+use std::fs::File;
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let selected_word = select_word();
+    println!("Selected word {}", selected_word);
+}
+
+fn select_word() -> String {
+    let mut file = File::open("words.txt").expect("could not open file!");
+
+    let mut file_contents = String::new();
+    file.read_to_string(&mut file_contents)
+        .expect("An error has occured while reading the file!");
+
+    let available_words : Vec<&str> = file_contents.trim().split(',').collect();
+
+    let rand_index = rand::thread_rng().gen_range(0..available_words.len());
+
+    return String::from(available_words[rand_index]);
 }
