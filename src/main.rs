b51ddaad1e51f31cf97d87f8c9e21597717441aa
Olakunle Dosunmu <olakunledosunmu@gmail.com>
2023-09-09 16:59:51 +0100

    creating letters and displaying user progress

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,16 @@ use rand::Rng;
 use std::fs::File;
 use std::io::prelude::*;
 
+struct Letter{
+    character: char,
+    revealed: bool
+}
+
 fn main() {
     let selected_word = select_word();
-    println!("Selected word {}", selected_word);
+    let mut letters = create_letters(&selected_word);
+
+    display_progress(&letters);
 }
 
 fn select_word() -> String {
@@ -21,4 +28,31 @@ fn select_word() -> String {
     let rand_index = rand::thread_rng().gen_range(0..available_words.len());
 
     return String::from(available_words[rand_index]);
+}
+
+fn create_letters(word: &String) -> Vec<Letter> {
+    let mut letters: Vec<Letter> = Vec::new();
+
+    for c in word.chars(){
+        letters.push(Letter { character: c, revealed: false });
+    }
+
+    return letters;
+}
+
+fn display_progress(letters: &Vec<Letter>) {
+    let mut display_string = String::from("Progress:");
+
+    for letter in letters{
+        display_string.push(' ');
+
+        if letter.revealed{
+            display_string.push(letter.character);
+        }
+        else{
+            display_string.push('_');
+        }
+        display_string.push(' ');
+    }
+    println!("{}",display_string);
 }
